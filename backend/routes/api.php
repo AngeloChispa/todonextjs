459fefbcba787f7a059f5c4bfee954bc8de5a86d
Jesus Nuñez <jn1910551@gmail.com>
2026-03-04 15:33:05 -0600

    cambios back

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -4,11 +4,14 @@ use App\Http\Controllers\NoteController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::apiResource('Notes', NoteController::class);
-
-Route::get('/user', function (Request $request) {
-    $user = Auth::user();
-    return response()->json($user);
-})->middleware("auth:sanctum");
 
 
+Route::middleware("auth:sanctum")->group(function () {
+    Route::get('/user', function (Request $request) {
+        $user = Auth::user();
+        return response()->json($user);
+    });
+
+    Route::apiResource('Notes', NoteController::class);
+
+});
